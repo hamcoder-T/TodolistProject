eb6a3f360af8a429836b992c0491d00cbe16eff5
Hamed <hamed.syntaxsavvy@gmail.com>
2025-11-01 09:50:24 +0330

    admin v3 -Renaming action functions

--- todolist/admin.py
+++ todolist/admin.py
@@ -7,17 +7,17 @@ admin.sites.AdminSite.site_title = "Admin"
 admin.sites.AdminSite.site_header = "پنل مدیریت سایت"
 
 
-def done_action(modeladmin, requst, queryset):
+def done_action_for_Event(modeladmin, requst, queryset):
     result = queryset.update(status='DO')
     modeladmin.message_user(requst, f"{result} event changed to Done status!")
 
 
-def undone_action(modeladmin, request, queryset):
+def undone_action_for_Event(modeladmin, request, queryset):
     result = queryset.update(status='UN')
     modeladmin.message_user(request, f"{result} event changed to Undone status !")
 
 
-def send_gmail(modeladmin, request, queryset):
+def send_gmail_for_Event(modeladmin, request, queryset):
     print(dir(queryset))
     modeladmin.message_user(request, f"{len(queryset)} reminder emails were sent to users.")
     for event in queryset:
@@ -30,9 +30,9 @@ def send_gmail(modeladmin, request, queryset):
             send_mail(subject, message_body, from_email, recipient_list, fail_silently=False)
 
 
-done_action.short_description = "change to done status"
-undone_action.short_description = "change to undone status"
-send_gmail.short_description = "send reminder gmail"
+done_action_for_Event.short_description = "change to done status"
+undone_action_for_Event.short_description = "change to undone status"
+send_gmail_for_Event.short_description = "send reminder gmail"
 
 
 # Register your models here.
@@ -41,7 +41,7 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ['user', 'title', 'short_description', 'begin', 'end', 'status']
     list_filter = ['status', 'user']
     autocomplete_fields = ['user']
-    actions = [done_action, undone_action, send_gmail]
+    actions = [done_action_for_Event, undone_action_for_Event, send_gmail_for_Event]
 
     # autocomplete_fields = ['user']
 
@@ -51,17 +51,17 @@ class EventAdmin(admin.ModelAdmin):
         return text[:20] + ('...' if len(text) > 20 else '')
 
 
-def done_action(modeladmin, requst, queryset):
+def done_action_for_Task(modeladmin, requst, queryset):
     result = queryset.update(is_done=True)
     modeladmin.message_user(requst, f"{result} event changed to Done status!")
 
 
-def undone_action(modeladmin, requst, queryset):
+def undone_action_for_Task(modeladmin, requst, queryset):
     result = queryset.update(is_done=False)
     modeladmin.message_user(requst, f"{result} event changed to Done status!")
 
 
-def send_gmail(modeladmin, request, queryset):
+def send_gmail_for_Task(modeladmin, request, queryset):
     modeladmin.message_user(request, f"{len(queryset)} reminder emails were sent to users.")
     for task in queryset:
         if task.user.email:
@@ -73,8 +73,8 @@ def send_gmail(modeladmin, request, queryset):
             send_mail(subject, message_body, from_email, recipient_list, fail_silently=False)
 
 
-done_action.short_description = "change to done status"
-undone_action.short_description = "change to undone status"
+done_action_for_Task.short_description = "change to done status"
+undone_action_for_Task.short_description = "change to undone status"
 
 
 @admin.register(Task)
@@ -82,7 +82,7 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ['user', 'title', 'short_description', 'is_done']
     list_filter = ['is_done']
     autocomplete_fields = ['user']
-    actions = []
+    actions = [done_action_for_Task, undone_action_for_Task]
 
     @admin.display(description='توضیحات (خلاصه)')
     def short_description(self, obj):
